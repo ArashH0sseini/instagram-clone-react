21896b7f6c5f9e3ac4ea165d2b6c52b4a1ff205e
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-25 15:29:23 +0430

    Building the Left Section of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
     <div>
-        
+        <div className='flex justify-between max-w-6xl'>
+        <div className='relative w-24 h-24 hidden lg:inline-grid cursor-pointer'>
+            <Image src="https://links.papareact.com/ocw" layout='fill'
+            objectFit='contain' alt='' />
+        </div>
+        <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
+        <Image src="https://links.papareact.com/jjm" layout='fill'
+            objectFit='contain' alt='' />
+        </div>
+        </div>
     </div>
   )
 }
