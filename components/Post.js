646aa909c35d9746f930f498ca978cd1643aa60e
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-27 15:33:35 +0430

    Building the Header Section of the Post

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-function Post({id,username,img,caption,userImg}) {
-  return (
-    <div>Post</div>
-  )
+function Post({ id, username, img, caption, userImg }) {
+    return (
+        <div className='bg-white my-7 border rounded-sm'>
+            {/* header */}
+            <div className='flex items-center p-5'>
+                <img src={img} className="rounded-full h-12 w-12 object-contain border p-1 mr-3" alt='' />
+                <p className='flex-1  font-bold'>{username}</p>
+                <EllipsisHorizontalIcon className='h-7' />
+            </div>
+
+            
+        </div>
+    )
 }
 
 export default Post
