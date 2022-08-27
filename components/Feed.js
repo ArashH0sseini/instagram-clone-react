8be4ef73ad98ec7af236c834c7b58766d5cc23ae
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-27 12:50:03 +0430

    Building the Stories Section of the Feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import Stories from './Stories'
 
 function Feed() {
-  return (
-    <main>
-        <section>
+    return (
+        <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
+         xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+            <section className='col-span-2'>
+                <Stories />
+            </section>
+            <section>
 
-        </section>
-        <section>
-            
-        </section>
-    </main>
-  )
+            </section>
+        </main>
+    )
 }
 
 export default Feed
