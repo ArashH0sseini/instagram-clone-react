3d68e45920d38e73eff2f40238305e46be23d22a
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-27 12:37:18 +0430

    Implementing Sticky Header

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import avatar from '../assets/avatar.jpg'
 
 function Header() {
     return (
-        <div>
+        <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
                 {/* Left */}
                 <div className='relative w-24 hidden lg:inline-grid cursor-pointer'>
