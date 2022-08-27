2af30face296c2bf7f1739755175c95c56f94600
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-27 14:34:06 +0430

    Implementing Faker.js

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
+import Story from './Story'
+
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([])
+
+  useEffect(() => {
+    const suggestions = [];
+
+    function createRandomUser() {
+      return {
+        userId: faker.datatype.uuid(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        avatar: faker.image.avatar(),
+        password: faker.internet.password(),
+        birthdate: faker.date.birthdate(),
+        registeredAt: faker.date.past(),
+      };
+    }
+
+    Array.from({ length: 20 }).map(() => {
+      suggestions.push(createRandomUser());
+    });
+
+
+    setSuggestions(suggestions)
+  }, []);
+
   return (
     <div>
-        
+      {suggestions.map(profile => (
+        <Story key={profile.userId} img={profile.avatar} username={profile.username} />
+      ))}
     </div>
   )
 }
