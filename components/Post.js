a7d6d0fd2bef5397807dcb048648e26127e2d69e
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-28 20:24:39 +0430

    Implementing the Comment Functionality

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
 import { HeartIcon, PaperAirplaneIcon, BookmarkIcon, ChatBubbleOvalLeftIcon, FaceSmileIcon } from '@heroicons/react/24/outline'
 import { EllipsisHorizontalIcon, HeartIcon as HeartIconsolid } from '@heroicons/react/24/solid'
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase'
 
 function Post({ id, username, img, caption, userImg }) {
     const { data: session } = useSession()
+    const [comment, setComment] = useState('')
+    const [comments, setComments] = useState([])
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(
+                    collection(db, 'posts', id, 'comments'),
+                    orderBy('timestamp', 'desc')
+                ),
+                (snapshot) => setComments(snapshot.docs)
+            ),
+        [db]
+    );
+
+    const sendComment = async (e) => {
+        e.preventDefault();
+
+        const commentToSend = comment;
+        setComment('');
+
+        await addDoc(collection(db, 'posts', id, 'comments'), {
+            comment: commentToSend,
+            username: session.user.username,
+            userImage: session.user.image,
+            timestamp: serverTimestamp()
+
+        })
+    }
+
+
     return (
         <div className='bg-white my-7 border rounded-sm'>
             {/* header */}
@@ -39,14 +72,22 @@ function Post({ id, username, img, caption, userImg }) {
                 </p>
             </div>
 
+            {/* comments */}
+
+
             {/* input box */}
             {session && (
                 <form className='flex items-center p-4'>
                     <FaceSmileIcon className='h-7' />
                     <input type="text"
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}
                         placeholder='Add a comment...'
                         className='border-none flex-1 focus:ring-0 outline-none' />
-                    <button className='font-semibold text-blue-400'>Post</button>
+                    <button type='submit'
+                        disabled={!comment.trim()}
+                        onClick={sendComment}
+                        className='font-semibold text-blue-400'>Post</button>
                 </form>
             )}
         </div>
