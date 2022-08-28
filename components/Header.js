7828627cd648943420b93b721b7de76d674aa9b0
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-28 10:08:35 +0430

    Complete NextAuth Authentication

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,22 @@ import React from 'react'
 import { useSession,signOut,signIn } from 'next-auth/react'
 import { GlobeEuropeAfricaIcon, HeartIcon, MagnifyingGlassIcon, PaperAirplaneIcon, PlusCircleIcon, PlusIcon } from '@heroicons/react/24/outline'
 import { HomeIcon } from '@heroicons/react/24/solid'
+import { useRouter } from 'next/router'
 
 function Header() {
     const { data: session } = useSession()
+    const router = useRouter()
 
     return (
         <div className='sticky top-0 z-50 bg-white border-b shadow-sm'>
             <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
                 {/* Left */}
                 <div className='relative hidden w-24 cursor-pointer lg:inline-grid'>
-                    <Image src="https://links.papareact.com/ocw" layout='fill'
+                    <Image onClick={()=>router.push('/')} src="https://links.papareact.com/ocw" layout='fill'
                         objectFit='contain' alt='' />
                 </div>
                 <div className='relative flex-shrink-0 w-10 cursor-pointer lg:hidden'>
-                    <Image src="https://links.papareact.com/jjm" layout='fill'
+                    <Image onClick={()=>router.push('/')} src="https://links.papareact.com/jjm" layout='fill'
                         objectFit='contain' alt='' />
                 </div>
 
@@ -33,7 +35,7 @@ function Header() {
 
                 {/* Right */}
                 <div className='flex items-center justify-end space-x-4'>
-                    <HomeIcon className='navBtn inline-flex' />
+                    <HomeIcon onClick={()=>router.push('/')} className='navBtn inline-flex' />
 
                     {session ? (
                         <>
@@ -46,7 +48,7 @@ function Header() {
                             <HeartIcon className='navBtn' />
 
                             <img onClick={signOut} src={session.user.image}  alt="" 
-                            className='h-8 w-8 rounded-full cursor-pointer' /></>
+                            className='h-10 w-10 rounded-full cursor-pointer' /></>
                     ) : (
                         <>
                             <button onClick={signIn}>Sign In</button>
--- components/Stories.js
+++ components/Stories.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker';
 import Story from './Story'
+import { useSession } from 'next-auth/react';
 
 
 function Stories() {
   const [suggestions, setSuggestions] = useState([])
+  const { data: session } = useSession()
 
   useEffect(() => {
     const suggestions = [];
@@ -31,6 +33,11 @@ function Stories() {
 
   return (
     <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-hide'>
+
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
+
       {suggestions.map(profile => (
         <Story key={profile.userId} img={profile.avatar} username={profile.username} />
       ))}
