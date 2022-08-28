8fbdf22e6070c450b483ee863bdef336b48b7ef9
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-28 10:33:17 +0430

    Implementing Recoil atoms

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import React from 'react'
-import { useSession,signOut,signIn } from 'next-auth/react'
+import { useSession, signOut, signIn } from 'next-auth/react'
 import { GlobeEuropeAfricaIcon, HeartIcon, MagnifyingGlassIcon, PaperAirplaneIcon, PlusCircleIcon, PlusIcon } from '@heroicons/react/24/outline'
 import { HomeIcon } from '@heroicons/react/24/solid'
 import { useRouter } from 'next/router'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 
 function Header() {
     const { data: session } = useSession()
+    const [open, setOpen] = useRecoilState(modalState)
     const router = useRouter()
 
     return (
@@ -15,11 +18,11 @@ function Header() {
             <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
                 {/* Left */}
                 <div className='relative hidden w-24 cursor-pointer lg:inline-grid'>
-                    <Image onClick={()=>router.push('/')} src="https://links.papareact.com/ocw" layout='fill'
+                    <Image onClick={() => router.push('/')} src="https://links.papareact.com/ocw" layout='fill'
                         objectFit='contain' alt='' />
                 </div>
                 <div className='relative flex-shrink-0 w-10 cursor-pointer lg:hidden'>
-                    <Image onClick={()=>router.push('/')} src="https://links.papareact.com/jjm" layout='fill'
+                    <Image onClick={() => router.push('/')} src="https://links.papareact.com/jjm" layout='fill'
                         objectFit='contain' alt='' />
                 </div>
 
@@ -35,7 +38,7 @@ function Header() {
 
                 {/* Right */}
                 <div className='flex items-center justify-end space-x-4'>
-                    <HomeIcon onClick={()=>router.push('/')} className='navBtn inline-flex' />
+                    <HomeIcon onClick={() => router.push('/')} className='navBtn inline-flex' />
 
                     {session ? (
                         <>
@@ -43,12 +46,12 @@ function Header() {
                                 <PaperAirplaneIcon className='-rotate-45 navBtn inline-flex' />
                                 <div className='absolute flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full -top-1 -right-1 animate-pulse'>3</div>
                             </div>
-                            <PlusCircleIcon className='navBtn' />
+                            <PlusCircleIcon onClick={() => setOpen(true)} className='navBtn' />
                             <GlobeEuropeAfricaIcon className='navBtn' />
                             <HeartIcon className='navBtn' />
 
-                            <img onClick={signOut} src={session.user.image}  alt="" 
-                            className='h-10 w-10 rounded-full cursor-pointer' /></>
+                            <img onClick={signOut} src={session.user.image} alt=""
+                                className='h-10 w-10 rounded-full cursor-pointer' /></>
                     ) : (
                         <>
                             <button onClick={signIn}>Sign In</button>
